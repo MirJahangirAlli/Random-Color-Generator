8a82b0102c5ce0aa1ac04f112afe9202b901a2cb
MIR JAHANGIR ALLI <mirjahangiralli33@gmail.com>
2023-12-06 18:47:43 +0530

    file

---
+++ RandomColor.js
@@ -0,0 +1,19 @@
+let btn=document.querySelector('button');
+let colorName=document.querySelector('h3');
+let div=document.querySelector('div');
+
+
+function getRandomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
+
+btn.addEventListener("click",function(){
+    let randomColor=getRandomColor(); 
+    colorName.innerText="Color Code : "+randomColor;
+    div.style.backgroundColor=randomColor;
+});
